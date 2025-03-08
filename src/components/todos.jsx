7280af5e073de7ab82409a00e0104a70e4c2613e
Bozhidar Zagorov <bozhidarzagorov@gmail.com>
2025-03-08 18:46:40 +0200

    add ideas and todo list

---
+++ src/components/todos.jsx
@@ -0,0 +1,18 @@
+//todo can make the login form from contacts page and redo contacts page to a map to indicate a building/email/phone number 
+//todo can use the little simbols for phone and mail
+//todo connect the server to the app to make register/login/logout
+//todo change catalog items by hardcoding it to a jsonfile with voblers (no prices just number of caught fish) 
+//todo can extract the img from google drive
+
+//todo change background to picture of dospat lake with the boat or find a big fishing boat with ppl with fishing gear or smt simmilar
+//todo it should be better with different pictures on most if not every page
+
+//todo reservation should be with a picture of the house in winter or with threes in snow
+//todo home page should be with a boat on water preferably snowy peaks
+
+//todo change the logo to a fish with angle of attack
+
+//todo add weather table for the next 7 or 14 days
+//! check if the api-key is still valid after experation //if inactive change to the other site key valid till 22 march
+
+//todo might make it so you can reserve the house (like booking) 
