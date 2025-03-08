5bfac895da48342a9767d8d7cc8660edc0c22971
Bozhidar Zagorov <bozhidarzagorov@gmail.com>
2025-03-08 17:55:57 +0200

    recoloring footer and rounding buttons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,33 +1,15 @@
-// export default function Footer(){
-//     return (
-//         <footer className="bg-blue-900 text-white py-6">
-//             <div className="container mx-auto flex flex-col md:flex-row items-center justify-between px-4">
-//                 <p className="text-sm">&copy; {new Date().getFullYear()} Your Company. All rights reserved.</p>
-//                 <ul className="flex space-x-6 mt-4 md:mt-0">
-//                     <li>
-//                         <a href="#" className="hover:underline">Privacy Policy</a>
-//                     </li>
-//                     <li>
-//                         <a href="#" className="hover:underline">Terms of Service</a>
-//                     </li>
-//                     <li>
-//                         <a href="#" className="hover:underline">Contact</a>
-//                     </li>
-//                 </ul>
-//             </div>
-//         </footer>
-//     )
-// }
+import { Link } from "react-router";
 
 export default function Footer(){
     return (
-        <footer className="bg-blue-900 text-white py-6 mt-auto">
+        <footer className="bg-[#0D1117] text-gray-400 py-6 mt-auto">
             <div className="container mx-auto flex flex-col md:flex-row items-center justify-between px-4">
-                <p className="text-sm">&copy; {new Date().getFullYear()} Your Company. All rights reserved.</p>
+                <p className="text-sm">&copy; {new Date().getFullYear()} Fishing&Living. All rights reserved.</p>
                 <ul className="flex space-x-6 mt-4 md:mt-0">
-                    <li><a href="#" className="hover:underline">Privacy Policy</a></li>
-                    <li><a href="#" className="hover:underline">Terms of Service</a></li>
-                    <li><a href="#" className="hover:underline">Contact</a></li>
+                    <li><Link to="#" className="btn-orange">Privacy Policy</Link></li>
+                    <li><Link to="#" className="btn-orange">Terms of Service</Link></li>
+                    <li><Link to="#" className="btn-orange">Contact</Link></li>
+                    {/* <Link to="/register" className="btn-orange"> */}
                 </ul>
             </div>
         </footer>
