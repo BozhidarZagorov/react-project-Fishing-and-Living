59b7e352a5244e77dbb380ec4cac61bb77a19bb0
Bozhidar Zagorov <bozhidarzagorov@gmail.com>
2025-03-11 16:00:02 +0200

    more ideas and change of plans for server

--- src/components/todos.jsx
+++ src/components/todos.jsx
@@ -3,6 +3,7 @@
 //todo connect the server to the app to make register/login/logout
 //todo change catalog items by hardcoding it to a jsonfile with voblers (no prices just number of caught fish) 
 //todo can extract the img from google drive
+//!change of plans all of above is to be done with firebase
 
 //todo change background to picture of dospat lake with the boat or find a big fishing boat with ppl with fishing gear or smt simmilar
 //todo it should be better with different pictures on most if not every page
@@ -14,6 +15,9 @@
 //todo change the logo to a fish with angle of attack
 
 //todo add weather table for the next 7 or 14 days
-//! check if the api-key is still valid after experation //if inactive change to the other site key valid till 22 march
+
+//todo add page with a boat and places for the rods and a dropdown menu for the possible attachable voblers on the current spot 
+    //todo add possible rods as well 
+    //todo to be possible to +1 and -1 caught fishes on the voubler (the -1 is for possible errors durring handling)
 
 //todo might make it so you can reserve the house (like booking) 
