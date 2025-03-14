0ff909c230d85df426ab7436ba63d2db146b9719
Bozhidar Zagorov <bozhidarzagorov@gmail.com>
2025-03-14 14:38:48 +0200

    fix if(city)
    added lastcity state to fix early return from city === city state=true

--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -6,11 +6,13 @@ export default function Weather() {
   const [weather, setWeather] = useState();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState();
+  const [lastCity, setLastCity] = useState(""); //last city
 
   const fetchWeather = async () => {
-    if (!city || city===city) return;
+    if (!city || city === lastCity) return;
     setLoading(true);
     setError();
+    setLastCity(city);
 
     try {
       const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&elements=datetime%2CdatetimeEpoch%2Cname%2Ctempmax%2Ctempmin%2Ctemp%2Chumidity%2Csnow%2Cwindspeed%2Cwindspeedmax%2Cwindspeedmin%2Cwinddir%2Cpressure%2Cuvindex%2Csunrise%2Csunset%2Cconditions%2Cdescription%2Cicon&include=days%2Chours%2Calerts%2Ccurrent&key=LQCFA6SWM9Q5TPK4T79HSX2U7&contentType=json`);
