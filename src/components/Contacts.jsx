d5147274ef764d25b24e886db4ea84d871fae02f
Bozhidar Zagorov <bozhidarzagorov@gmail.com>
2025-03-15 18:55:00 +0200

    change form and useRef

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,21 +1,41 @@
-'use client'
-
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import { useNavigate } from 'react-router'
-import { ChevronDownIcon } from '@heroicons/react/16/solid'
+// import { ChevronDownIcon } from '@heroicons/react/16/solid'
 import { Field, Label, Switch } from '@headlessui/react'
 
 export default function About() {
     const [agreed, setAgreed] = useState(false)
     const navigate = useNavigate();
 
-    const formAction = (formData) => {
-        console.log(Object.fromEntries(formData));
+    const firstNameRef = useRef();
+    const lastNameRef = useRef();
+    const emailRef = useRef();
+    const phoneRef = useRef()
+    const messageRef = useRef();
 
-        console.log('Sending the contact information...');
+    const handleSubmit = async (e) => {
+        e.preventDefault();
 
-        navigate('/');
+        const formData = {
+            firstName: firstNameRef.current.value,
+            lastName: lastNameRef.current.value,
+            email: emailRef.current.value, // might remove it after all they log in with email
+            phone: phoneRef.current.value,
+            message: messageRef.current.value,
+        };
+
+        console.log("Sending email with:", formData);
+    
+        
     }
+
+    // const formAction = (formData) => {
+    //     console.log(Object.fromEntries(formData));
+
+    //     console.log('Sending the contact information...');
+
+    //     navigate('/');
+    // }
 
 
     return (
@@ -36,7 +56,8 @@ export default function About() {
                 <h2 className="text-4xl font-semibold tracking-tight text-balance text-gray-900 sm:text-5xl">Contact sales</h2>
                 <p className="mt-2 text-lg/8 text-gray-600">Aute magna irure deserunt veniam aliqua magna enim voluptate.</p>
             </div>
-            <form action={formAction} className="mx-auto mt-16 max-w-xl sm:mt-20">
+            <form onSubmit={handleSubmit} className="mx-auto mt-16 max-w-xl sm:mt-20">
+            {/* <form action={formAction} className="mx-auto mt-16 max-w-xl sm:mt-20"> */}
                 <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
                     <div>
                         <label htmlFor="first-name" className="block text-sm/6 font-semibold text-gray-900">
@@ -47,7 +68,9 @@ export default function About() {
                                 id="first-name"
                                 name="first-name"
                                 type="text"
+                                required
                                 autoComplete="given-name"
+                                ref={firstNameRef}
                                 className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
                             />
                         </div>
@@ -61,12 +84,14 @@ export default function About() {
                                 id="last-name"
                                 name="last-name"
                                 type="text"
+                                required
                                 autoComplete="family-name"
+                                ref={lastNameRef}
                                 className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
                             />
                         </div>
                     </div>
-                    <div className="sm:col-span-2">
+                    {/* <div className="sm:col-span-2">
                         <label htmlFor="company" className="block text-sm/6 font-semibold text-gray-900">
                             Company
                         </label>
@@ -79,7 +104,7 @@ export default function About() {
                                 className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
                             />
                         </div>
-                    </div>
+                    </div> */}
                     <div className="sm:col-span-2">
                         <label htmlFor="email" className="block text-sm/6 font-semibold text-gray-900">
                             Email
@@ -89,7 +114,9 @@ export default function About() {
                                 id="email"
                                 name="email"
                                 type="email"
+                                required
                                 autoComplete="email"
+                                ref={emailRef}
                                 className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
                             />
                         </div>
@@ -100,7 +127,7 @@ export default function About() {
                         </label>
                         <div className="mt-2.5">
                             <div className="flex rounded-md bg-white outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600">
-                                <div className="grid shrink-0 grid-cols-1 focus-within:relative">
+                                {/* <div className="grid shrink-0 grid-cols-1 focus-within:relative">
                                     <select
                                         id="country"
                                         name="country"
@@ -108,6 +135,7 @@ export default function About() {
                                         aria-label="Country"
                                         className="col-start-1 row-start-1 w-full appearance-none rounded-md py-2 pr-7 pl-3.5 text-base text-gray-500 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                                     >
+                                        <option>BG</option>
                                         <option>US</option>
                                         <option>CA</option>
                                         <option>EU</option>
@@ -116,12 +144,14 @@ export default function About() {
                                         aria-hidden="true"
                                         className="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4"
                                     />
-                                </div>
+                                </div> */}
                                 <input
                                     id="phone-number"
                                     name="phone-number"
                                     type="text"
+                                    required
                                     placeholder="123-456-7890"
+                                    ref={phoneRef}
                                     className="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
                                 />
                             </div>
@@ -136,7 +166,9 @@ export default function About() {
                                 id="message"
                                 name="message"
                                 rows={4}
+                                required
                                 className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                                ref={messageRef}
                                 defaultValue={''}
                             />
                         </div>
