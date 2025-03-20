21b30a2586364f1f54f90508ff4031abc5917d79
Bozhidar Zagorov <bozhidarzagorov@gmail.com>
2025-03-20 23:39:53 +0200

    fetching apartments from firebase db

--- src/components/Apartments.jsx
+++ src/components/Apartments.jsx
@@ -1,6 +1,11 @@
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router'
+import { collection, getDocs} from 'firebase/firestore'
+import { db } from '../../config/firebaseinit'
+import { useAuth } from '../../ctx/FirebaseAuth'
+
 import { CheckIcon } from '@heroicons/react/20/solid'
 import { UserIcon } from '@heroicons/react/24/solid';
-import { Link } from 'react-router'
 
 const tiers = [
     {
@@ -46,6 +51,31 @@ function classNames(...classes) {
 }
 
 export default function Apartments() {
+    const [apartments, setApartments] = useState({})
+    const [loading, setLoading] = useState(true)
+    const { user, isAuthenticated } = useAuth(); //! auth ctx
+
+
+    useEffect(() => {
+
+        const fetchApartment = async () => {
+        try{
+            const querySnapshot = await getDocs(collection(db, "apartments"));
+                const apartmentList = querySnapshot.docs.map(doc => ({
+                    id: doc.id,
+                    ...doc.data()
+                }));
+                setApartments(apartmentList);
+        }catch(error){
+            console.error('Error fetching apartment:', error)
+        }
+            setLoading(false);
+        };
+        fetchApartment();
+    }, []);
+
+    if (loading) return <p>Loading...</p>;// add proper loading
+
     return (
         <div className="relative isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
         <>
@@ -67,63 +97,62 @@ export default function Apartments() {
                     For your ultimate fishing and living experience.
             </p>
             <div className="mx-auto mt-16 grid max-w-lg grid-cols-1 items-center gap-y-6 sm:mt-20 sm:gap-y-0 lg:max-w-4xl lg:grid-cols-2">
-                {tiers.map((tier, tierIdx) => (
+                {apartments.map((apartment,idx) => (
                     <div
-                        key={tier.id}
+                        key={apartment.key}
                         className={classNames(
-                            tier.featured ? 'relative bg-gray-900 shadow-2xl' : 'bg-white/60 sm:mx-8 lg:mx-0',
-                            tier.featured
+                            apartment.featured ? 'relative bg-gray-900 shadow-2xl' : 'bg-white/60 sm:mx-8 lg:mx-0',
+                            apartment.featured
                                 ? ''
-                                : tierIdx === 0
+                                : idx === 0
                                     ? 'rounded-t-3xl sm:rounded-b-none lg:rounded-tr-none lg:rounded-bl-3xl'
                                     : 'sm:rounded-t-none lg:rounded-tr-3xl lg:rounded-bl-none',
                             'rounded-3xl p-8 ring-1 ring-gray-900/10 sm:p-10',
                         )}
                     >
                         <h3
-                            id={tier.id}
-                            className={classNames(tier.featured ? 'text-indigo-400' : 'text-indigo-600', 'text-base/7 font-semibold')}
+                            id={apartment.key}
+                            className={classNames(apartment.featured ? 'text-indigo-400' : 'text-indigo-600', 'text-base/7 font-semibold')}
                         >
-                            {tier.name}
+                            {apartment.name}
                         </h3>
                         <p className="mt-4 flex items-baseline gap-x-2">
                             <span
                                 className={classNames(
-                                    tier.featured ? 'text-white' : 'text-gray-900',
+                                    apartment.featured ? 'text-white' : 'text-gray-900',
                                     'text-5xl font-semibold tracking-tight',
                                 )}
                             >
-                                {tier.priceMonthly}
+                                {apartment.priceDaily}
                             </span>
-                            <span className={classNames(tier.featured ? 'text-gray-400' : 'text-gray-500', 'text-base')}>
+                            <span className={classNames(apartment.featured ? 'text-gray-400' : 'text-gray-500', 'text-base')}>
                                 leva<UserIcon className="inline-block w-5 h-5 text-gray-500 ml-1" /> / day
                             </span>
                         </p>
-                        <p className={classNames(tier.featured ? 'text-gray-300' : 'text-gray-600', 'mt-6 text-base/7')}>
-                            {tier.description}
+                        <p className={classNames(apartment.featured ? 'text-gray-300' : 'text-gray-600', 'mt-6 text-base/7')}>
+                            {apartment.description}
                         </p>
                         <ul
                             role="list"
                             className={classNames(
-                                tier.featured ? 'text-gray-300' : 'text-gray-600',
+                                apartment.featured ? 'text-gray-300' : 'text-gray-600',
                                 'mt-8 space-y-3 text-sm/6 sm:mt-10',
                             )}
                         >
-                            {tier.features.map((feature) => (
-                                <li key={feature} className="flex gap-x-3">
+                            {apartment.features.map((feature,index) => (
+                                <li key={index} className="flex gap-x-3">
                                     <CheckIcon
                                         aria-hidden="true"
-                                        className={classNames(tier.featured ? 'text-indigo-400' : 'text-indigo-600', 'h-6 w-5 flex-none')}
+                                        className={classNames(apartment.featured ? 'text-indigo-400' : 'text-indigo-600', 'h-6 w-5 flex-none')}
                                     />
                                     {feature}
                                 </li>
                             ))}
                         </ul>
                         <Link
-                            to={tier.href}
-                            aria-describedby={tier.id}
+                            to={apartment.href}
                             className={classNames(
-                                tier.featured
+                                apartment.featured
                                     ? 'bg-indigo-500 text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-indigo-500'
                                     : 'text-indigo-600 ring-1 ring-indigo-200 ring-inset hover:ring-indigo-300 focus-visible:outline-indigo-600',
                                 'mt-8 block rounded-md px-3.5 py-2.5 text-center text-sm font-semibold focus-visible:outline-2 focus-visible:outline-offset-2 sm:mt-10',
