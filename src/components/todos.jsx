cee0bbe0d87d4f86e6a187cf9683b8fc68667c7d
Bozhidar Zagorov <bozhidarzagorov@gmail.com>
2025-03-25 23:20:05 +0200

    change Home background and move style to css file

--- src/components/todos.jsx
+++
@@ -1,9 +0,0 @@
-//todo can extract the img from google drive
-
-//todo change background to picture of dospat lake with the boat or find a big fishing boat with ppl with fishing gear or smt simmilar
-//todo it should be better with different pictures on most if not every page
-//todo homepage could be 1st half of the house and 2nd on the water with the boat or smt like that
-
-//todo add page with a boat and places for the rods and a dropdown menu for the possible attachable voblers on the current spot 
-    //todo add possible rods as well 
-    //todo to be possible to +1 and -1 caught fishes on the voubler (the -1 is for possible errors durring handling)
