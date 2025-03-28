64e795b5eea5cd9dd40656698faa4be96bfba105
Bozhidar Zagorov <bozhidarzagorov@gmail.com>
2025-03-28 13:19:41 +0200

    privacy policy

---
+++ src/components/PrivacyPolicy.jsx
@@ -0,0 +1,49 @@
+export default function PrivacyPolicy() {
+  return (
+    <div className="mt-20 max-w-4xl mx-auto px-6 py-12">
+      <h1 className="text-4xl font-bold mb-6">Privacy Policy</h1>
+      <p className="mb-4">
+        This Privacy Policy outlines how we collect, use, and protect your
+        personal data when using our Fishing&Living app.
+      </p>
+
+      <h2 className="text-2xl font-semibold mt-6">
+        1. Data Collection
+      </h2>
+      <p>
+        We collect user data such as email addresses, images uploaded, and
+        interaction data for improving our services.
+      </p>
+
+      <h2 className="text-2xl font-semibold mt-6">2. Use of Information</h2>
+      <p>
+        Your data is used for app functionality, personalized experiences, and
+        service improvement.
+      </p>
+
+      <h2 className="text-2xl font-semibold mt-6">3. Data Sharing</h2>
+      <p>
+        We do not sell your data. It is only shared with third-party services
+        necessary for app functionality, such as Cloudinary for image hosting and Firebase.
+      </p>
+
+      <h2 className="text-2xl font-semibold mt-6">
+        4. Security Measures
+      </h2>
+      <p>
+        We take security seriously and implement measures to protect your
+        personal data.
+      </p>
+
+      <h2 className="text-2xl font-semibold mt-6">5. User Rights</h2>
+      <p>
+        You have the right to request data deletion or modification at any
+        time. Contact us through the contact page.
+      </p>
+
+      <p className="text-3xl font-bold mt-6">
+        By using our app, you agree to the terms in this Privacy Policy.
+      </p>
+    </div>
+  );
+}
